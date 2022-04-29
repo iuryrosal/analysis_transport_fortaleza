f8953375130bb0bc1510b983143dc09ea24427a8
Iury Lima Rosal <iuryrosal@gmail.com>
2022-04-28 22:09:53 -0300

    ✨ feat: Adiciona função que ler vários arquivos em um diretorio

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,15 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import glob
+import pandas as pd
 
+def read_files(files, index, df_array):
+    if index >= len(files):
+        return pd.concat(df_array, axis = 0)
+    else:
+        df_temp = function(files[index])
+        df_array.append(df_temp)
+        return read_files(files, index + 1, df_array)
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def make_csv(directory):
+    dir_ = directory + "*.xml"
+    return glob.glob(dir_)
 
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
