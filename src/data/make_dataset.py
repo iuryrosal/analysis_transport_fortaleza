ca03f5a60a66b2ad7b6b0f5431010c1faba9281e
Iury Lima Rosal <iuryrosal@gmail.com>
2022-04-28 22:17:11 -0300

    ♻️ refactor: Altera o nome das funções make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,15 +1,14 @@
 import glob
 import pandas as pd
 
-def read_files(files, index, df_array):
+def convert_files(files, index, df_array):
     if index >= len(files):
         return pd.concat(df_array, axis = 0)
     else:
         df_temp = function(files[index])
         df_array.append(df_temp)
-        return read_files(files, index + 1, df_array)
+        return convert_files(files, index + 1, df_array)
 
-def make_csv(directory):
+def pick_files(directory):
     dir_ = directory + "*.xml"
     return glob.glob(dir_)
-
