05c9dd2b77ca083c96c3dbd7384b430dc2842848
Iury Lima Rosal <iuryrosal@gmail.com>
2022-05-05 20:45:01 -0300

    chore: Adiciona / no começo do .xml para evitar problemas de acesso ao diretorio

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,7 @@
 import glob
 import pandas as pd
 
-def convert_files(files, index, df_array):
+def convert_files(files: list, index: int = 0, df_array: list = []):
     if index >= len(files):
         return pd.concat(df_array, axis = 0)
     else:
@@ -10,5 +10,5 @@ def convert_files(files, index, df_array):
         return convert_files(files, index + 1, df_array)
 
 def pick_files(directory):
-    dir_ = directory + "*.xml"
+    dir_ = directory + "/*.xml"
     return glob.glob(dir_)
