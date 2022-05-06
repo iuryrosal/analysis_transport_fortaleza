80caeb3ef6880e5ab9a7491d94df2de06770c101
Iury Lima Rosal <iuryrosal@gmail.com>
2022-05-06 10:41:25 -0300

    ✨ chore: Adiciona script para leitura de diretorio e construção de dataframe

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,14 +1,45 @@
 import glob
 import pandas as pd
+from psutil import disk_partitions
+from conver_gr import convert_gr_file
+from conver_v import convert_v_file
+import logging
 
-def convert_files(files: list, index: int = 0, df_array: list = []):
+logging.basicConfig(filename = "log_processamento.log",
+                    level = logging.DEBUG,
+                    format = "%(asctime)s :: %(message)s :: %(levelno)s :: %(lineno)d")
+
+def convert_files(files: list, index: int = 0, df_array: list = [], type_file = "gr"):
     if index >= len(files):
         return pd.concat(df_array, axis = 0)
     else:
-        df_temp = function(files[index])
+        logging.info("Processando: " + files[index])
+        if type_file == "gr":
+            df_temp = convert_gr_file(files[index])
+        elif type_file == "v":
+            df_temp = convert_v_file(files[index])
+        else:
+            df_temp = convert_gr_file(files[index])
+
         df_array.append(df_temp)
         return convert_files(files, index + 1, df_array)
 
 def pick_files(directory):
-    dir_ = directory + "/*.xml"
-    return glob.glob(dir_)
+    return glob.glob(directory)
+
+def make_dataset():
+    logging.info("Processamento GR Iniciado")
+    files = pick_files("../../data/raw/gr/GR_*.xml")
+    df_gr = convert_files(files = files,
+                          type_file="gr")
+    df_gr.to_csv("../../data/processed/GR.csv")
+    logging.info("Processamento GR Concluído")
+
+    logging.info("Processamento V Iniciado")
+    files = pick_files("../../data/raw/v/V*.xml")
+    df_gr = convert_files(files = files,
+                          type_file="v")
+    df_gr.to_csv("../../data/processed/V.csv")
+    logging.info("Processamento V Concluído")
+
+make_dataset()
