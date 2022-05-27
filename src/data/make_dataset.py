fabf2686515b6638184d884ae02a2cf4f17586da
Iury Lima Rosal <iuryrosal@gmail.com>
2022-05-26 21:46:00 -0300

    docs: Adiciona jupyter notebook com analise inicial das 2 primeiras hipóteses

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,7 @@ import pandas as pd
 from psutil import disk_partitions
 from conver_gr import convert_gr_file
 from conver_v import convert_v_file
+from conver_fgr import convert_fgr_file
 import logging
 
 logging.basicConfig(filename = "log_processamento.log",
@@ -19,7 +20,7 @@ def convert_files(files: list, index: int = 0, df_array: list = [], type_file = 
         elif type_file == "v":
             df_temp = convert_v_file(files[index])
         else:
-            df_temp = convert_gr_file(files[index])
+            df_temp = convert_fgr_file(files[index])
 
         df_array.append(df_temp)
         return convert_files(files, index + 1, df_array)
@@ -28,12 +29,12 @@ def pick_files(directory):
     return glob.glob(directory)
 
 def make_dataset():
-    logging.info("Processamento GR Iniciado")
-    files = pick_files("../../data/raw/gr/GR_*.xml")
-    df_gr = convert_files(files = files,
-                          type_file="gr")
-    df_gr.to_csv("../../data/processed/GR.csv")
-    logging.info("Processamento GR Concluído")
+    #logging.info("Processamento GR Iniciado")
+    #files = pick_files("../../data/raw/gr/GR_*.xml")
+    #df_gr = convert_files(files = files,
+    #                      type_file="gr")
+    #df_gr.to_csv("../../data/processed/GR.csv")
+    #logging.info("Processamento GR Concluído")
 
     logging.info("Processamento V Iniciado")
     files = pick_files("../../data/raw/v/V*.xml")
